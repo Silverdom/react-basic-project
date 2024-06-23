f69a11c0949edd9b78d97c57e573daf1dc3d8904
SohamE <soham.584sengupta@gmail.com>
2024-06-23 20:38:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "rsuite";
+
+function App() {
+  return (
+    <>
+      <Button color="cyan" appearance="primary">
+        Hello
+      </Button>
+      <h1 className="text-red-500">Hi</h1>;
+    </>
+  );
+}
+
+export default App;
